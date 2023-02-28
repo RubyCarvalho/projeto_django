df91bec7e74c43df5c4a221d88597a35ce148435
ander <ruby.bandeira@ifam.edu.br>
2023-02-28 14:02:59 -0400

    adicionando arquivo ao repo

--- recipes/views.py
+++ recipes/views.py
@@ -4,5 +4,4 @@ from django.shortcuts import render
 def home(request):
     return render(request, 'recipes/pages/home.html', context={
         'name': 'Ruby Carvalho',
-        'idade': '23',
     })
