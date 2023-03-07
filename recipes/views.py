667d456e3d771e5eb43481c6dcc04fc44af1ffe1
ander <ruby.bandeira@ifam.edu.br>
2023-03-07 12:19:10 -0400

    finalizando templates: herança, blocos, if e for

--- recipes/views.py
+++ recipes/views.py
@@ -1,13 +1,15 @@
 from django.shortcuts import render
+from utils.recipes.factory import make_recipe
 
 
 def home(request):
     return render(request, 'recipes/pages/home.html', context={
-        'name': 'Ruby Carvalho',
+        'recipes': [make_recipe() for _ in range(2)],
     })
 
 
 def recipe(request, id):
     return render(request, 'recipes/pages/recipe-view.html', context={
-        'name': 'Ruby Carvalho',
+        'recipe': make_recipe(),
+        'is_detail_page': True,
     })
