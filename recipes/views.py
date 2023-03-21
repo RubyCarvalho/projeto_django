0e61e95326612962ff549579ca218099d13877c4
ander <ruby.bandeira@ifam.edu.br>
2023-03-21 15:33:41 -0400

    Explorando a (seção 9), seção dedicada a testes. Configurando o 'pytest'.

--- recipes/views.py
+++ recipes/views.py
@@ -24,7 +24,7 @@ def category(request, category_id):
     )
     return render(request, 'recipes/pages/category.html', context={
         'recipes': recipes,
-        'title': f'{recipes[0].category.name} - Category | '
+        'title': f'{recipes[0].category.name} - categoria | '
     })
 
 
