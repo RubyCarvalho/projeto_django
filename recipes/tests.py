96c4b762726f1a51d386c93ac3585251bdf228bf
ander <ruby.bandeira@ifam.edu.br>
2023-03-27 10:26:31 -0400

    Atualizando o repo, adicionando modificações as settings do site

--- recipes/tests.py
+++ recipes/tests.py
@@ -7,6 +7,10 @@ class RecipeURLsTest(TestCase):
         home_url = reverse('recipes:home')
         self.assertEqual(home_url, '/')
 
+    def test_recipe_category_url_is_correct(self):
+        url = reverse('recipes:category', kwargs={'category_id': 1})
+        self.assertEqual(url, '/recipes/category/1/')
 
-class RecipeViewsTest(TestCase):
-    ...
+    def test_recipe_detail_url_is_correct(self):
+        url = reverse('recipes:recipe', kwargs={'id': 1})
+        self.assertEqual(url, '/recipes/1/')
