3877acc7a3deee1935dcc51d1a2838052db133cc
ander <ruby.bandeira@ifam.edu.br>
2023-04-10 14:55:41 -0400

    Adicionando modifacações ao template do site

--- utils/test_pagination.py
+++ utils/test_pagination.py
@@ -4,6 +4,7 @@ from utils.pagination import make_pagination_range
 
 
 class PaginationTest(TestCase):
+
     def test_make_pagination_range_returns_a_pagination_range(self):
         pagination = make_pagination_range(
             page_range=list(range(1, 21)),
@@ -44,7 +45,7 @@ class PaginationTest(TestCase):
             page_range=list(range(1, 21)),
             qty_pages=4,
             current_page=4,
-        )
+        )['pagination']
         self.assertEqual([3, 4, 5, 6], pagination)
 
     def test_make_sure_middle_ranges_are_correct(self):
