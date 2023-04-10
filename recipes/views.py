48eab66ab0af8dd48780b75c0dcad6923393e293
ander <ruby.bandeira@ifam.edu.br>
2023-04-10 14:09:02 -0400

    finalizando a 9seção 11)

--- recipes/views.py
+++ recipes/views.py
@@ -1,3 +1,4 @@
+from django.core.paginator import Paginator
 from django.db.models import Q
 from django.http import Http404
 from django.shortcuts import get_list_or_404, get_object_or_404, render
@@ -6,14 +7,16 @@ from recipes.models import Recipe
 
 
 def home(request):
-    recipes = get_list_or_404(
-        Recipe.objects.filter(
-            is_published=True
-        ).order_by('-id')
-    )
+    recipes = Recipe.objects.filter(
+        is_published=True,
+    ).order_by('-id')
+
+    current_page = request.GET.get('page', 1)
+    paginator = Paginator(recipes, 9)
+    page_obj = paginator.get_page(current_page)
 
     return render(request, 'recipes/pages/home.html', context={
-        'recipes': recipes,
+        'recipes': page_obj,
     })
 
 
@@ -54,5 +57,5 @@ def search(request):
     return render(request, 'recipes/pages/search.html', {
         'page_title': f'Procurar "{search_term}" ',
         'search_term': search_term,
-        'recipes': recipes
+        'recipes': recipes,
     })
