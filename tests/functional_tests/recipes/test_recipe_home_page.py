40ea8633fa320731b0fcf8503152c791da25a0ef
ander <ruby.bandeira@ifam.edu.br>
2023-04-26 17:54:26 -0400

    Testando formulario de login

--- tests/functional_tests/recipes/test_recipe_home_page.py
+++ tests/functional_tests/recipes/test_recipe_home_page.py
@@ -1,3 +1,4 @@
+
 from unittest.mock import patch
 
 import pytest
@@ -14,32 +15,51 @@ class RecipeHomePageFunctionalTest(RecipeBaseFunctionalTest):
         body = self.browser.find_element(By.TAG_NAME, 'body')
         self.assertIn('No recipes found here 🥲', body.text)
 
+    @patch('recipes.views.PER_PAGE', new=2)
+    def test_recipe_search_input_can_find_correct_recipes(self):
+        recipes = self.make_recipe_in_batch()
 
-@patch('recipes.views.PER_PAGE', new=2)
-def test_recipe_search_input_can_find_correct_recipes(self):
-    recipes = self.make_recipe_in_batch()
+        title_needed = 'This is what I need'
 
-    title_needed = 'This is what I need'
+        recipes[0].title = title_needed
+        recipes[0].save()
 
-    recipes[0].title = title_needed
-    recipes[0].save()
+        # Usuário abre a página
+        self.browser.get(self.live_server_url)
 
-    # Usuário abre a página
-    self.browser.get(self.live_server_url)
+        # Vê um campo de busca com o texto "Search for a recipe"
+        search_input = self.browser.find_element(
+            By.XPATH,
+            '//input[@placeholder="Search for a recipe"]'
+        )
 
-    # Vê um campo de busca com o texto "Search for a recipe"
-    search_input = self.browser.find_element(
-        By.XPATH,
-        '//input[@placeholder="Search for a recipe"]'
-    )
+        # Clica neste input e digita o termo de busca
+        # para encontrar a receita o título desejado
+        search_input.send_keys(title_needed)
+        search_input.send_keys(Keys.ENTER)
 
-    # Clica neste input e digita o termo de busca
-    # para encontrar a receita o título desejado
-    search_input.send_keys(title_needed)
-    search_input.send_keys(Keys.ENTER)
+        # O usuário vê o que estava procurando na página
+        self.assertIn(
+            title_needed,
+            self.browser.find_element(By.CLASS_NAME, 'main-content-list').text,
+        )
 
-    # O usuário vê o que estava procurando na página
-    self.assertIn(
-        title_needed,
-        self.browser.find_element(By.CLASS_NAME, 'main-content-list').text,
-    )
+    @patch('recipes.views.PER_PAGE', new=2)
+    def test_recipe_home_page_pagination(self):
+        self.make_recipe_in_batch()
+
+        # Usuário abre a página
+        self.browser.get(self.live_server_url)
+
+        # Vê que tem uma paginação e clica na página 2
+        page2 = self.browser.find_element(
+            By.XPATH,
+            '//a[@aria-label="Go to page 2"]'
+        )
+        page2.click()
+
+        # Vê que tem mais 2 receitas na página 2
+        self.assertEqual(
+            len(self.browser.find_elements(By.CLASS_NAME, 'recipe')),
+            2
+        )
