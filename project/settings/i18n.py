3324ba9fda9ffb09de98be900f7ffa17e69f77f8
ander <ruby.bandeira@ifam.edu.br>
2023-05-18 17:43:14 -0400

    Atualizando repo

--- project/settings/i18n.py
+++ project/settings/i18n.py
@@ -1,10 +1,12 @@
-# Internationalization
-# https://docs.djangoproject.com/en/3.2/topics/i18n/
+from . import BASE_DIR
 
 LANGUAGE_CODE = 'pt-br'
 
 TIME_ZONE = 'America/Sao_Paulo'
-
-USE_I18N = True
+USE_I18N = False
 
 USE_TZ = True
+
+LOCALE_PATHS = [
+    BASE_DIR / 'locale',
+]
